9c77e326edb176f487cd77c53c5becacf980c006
Terry Geunbin Kim <terry.kim@nexr.com>
2022-04-09 17:17:33 +0900

    - favicon 추가 - login 처리 테스트

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,10 +3,31 @@ import {Button, Form, Input} from "antd";
 import ndap_bi from '../../assets/login_bi.png';
 
 const LoginForm = () => {
-    const onLogin = () => {
+    const [submitLoading, setSubmitLoading] = useState(false);
 
+    const onLogin = () => {
+        setSubmitLoading(true);
+
+        fetch('http://localhost:8001/security/login', {
+            method : 'POST',
+            body : JSON.stringify({
+                userName : id,
+                password : password
+            })
+        })
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((e) => {
+                console.error(`error : !!!`, e);
+            })
+            .finally(() => {
+                setSubmitLoading(false);
+            })
+        ;
     };
-    
+
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
 
@@ -18,17 +39,24 @@ const LoginForm = () => {
         setPassword(e.target.value);
     }, []);
 
-
-
     return (
         <Form onFinish={onLogin}>
-            <img src={ndap_bi}/>
-            <br/>
-            <Input placeholder="User ID" onChange={onChangeId} value={id} />
-            <br/>
-            <Input placeholder="Password" onChange={onChangePassword} value={password} />
-            <br/>
-            <Button type="primary">Login</Button>
+            <Form.Item>
+                <img src={ndap_bi}/>
+            </Form.Item>
+            <Form.Item
+                name="username"
+            >
+                <Input placeholder="User ID" onChange={onChangeId} value={id} />
+            </Form.Item>
+            <Form.Item
+                name="password"
+            >
+                <Input.Password placeholder="Password" onChange={onChangePassword} value={password} />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" loading={submitLoading}>Login</Button>
+            </Form.Item>
         </Form>
     );
 };
