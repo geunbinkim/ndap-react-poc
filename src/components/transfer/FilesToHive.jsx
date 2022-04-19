1c03e544e5b7fb3221be080d351a97567b59b1cb
Terry Geunbin Kim <terry.kim@nexr.com>
2022-04-19 22:59:35 +0900

    push

--- src/components/transfer/FilesToHive.jsx
+++ src/components/transfer/FilesToHive.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {Form, Input, Select} from 'antd';
 
 const FilesToHive = () => {
     const dummy_database = [{"name":"default","bookmarked":false},{"name":"iceberg","bookmarked":false},{"name":"noaa","bookmarked":false},{"name":"phoenix","bookmarked":false},{"name":"qc3","bookmarked":false},{"name":"spark","bookmarked":false}];
     const { Option } = Select;
+    const [tableSelectDisabled, setTableSelectDisabled] = useState(true);
+    const onSelect = useCallback((e) => {
+        setTableSelectDisabled(!!!e);
+    }, [Select])
     return (
         <>
             <h2>Target Table</h2>
             <Form.Item label="Target Database">
-                <Select>
+                <Select onSelect={onSelect}>
+                    <option value={null}>choose</option>
                     {
                         dummy_database.map((database) => {
                             return <Option key={database.name} value={database.name}>{database.name}</Option>
@@ -17,7 +22,7 @@ const FilesToHive = () => {
                 </Select>
             </Form.Item>
             <Form.Item label="Target Table">
-                <Select>
+                <Select disabled={tableSelectDisabled}>
                     {
                         dummy_database.map((database) => {
                             return <Option key={database.name} value={database.name}>{database.name}</Option>
